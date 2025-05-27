fcfa509588c25ed2e8c9ebceba57acf4e849b40a
viktovbaluev47007 <viktovbaluev34@gmail.com>
2025-05-27 16:48:38 +0500

    Update MainActivity.kt

--- app/src/main/app/src/main/java/com/example/lumi/MainActivity.kt
+++ app/src/main/app/src/main/java/com/example/lumi/MainActivity.kt
@@ -6,6 +6,6 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // Пока без макета
+        setContentView(R.layout.activity_main)
     }
 }
